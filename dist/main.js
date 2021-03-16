36534d396638278307b8dd3365a251c2266429aa
Mike Harvey <mikeharvey@mikes-work-mbp.lan>
2021-03-16 14:32:17 -0600

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("Tyler likes cats!"),console.log(void 0)})();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {tyler, bruner, cade} from 'js-library'
+
+tyler()
+// cade()
+console.log(bruner(false))
